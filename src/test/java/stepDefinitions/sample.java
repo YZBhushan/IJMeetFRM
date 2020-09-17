7c44e1780f660ba79337da3fe1e6ac9012ecffdb
YZBhushan <yzbhushan@gmail.com>
2020-09-17 12:11:28 +0530

    first commit

---
+++ src/test/java/stepDefinitions/sample.java
@@ -0,0 +1,10 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+
+public class sample {
+	@Given("User is on Home Page")
+	public void user_is_on_Home_Page() {
+	  System.out.println("#####################ok################");
+	}
+}
